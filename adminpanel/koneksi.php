26083e122878d8a2a30efdd461015c2da1f8b402
aisyahsalshabilla <125172647+aisyahsalshabilla@users.noreply.github.com>
2023-12-01 11:30:27 +0700

    hapus file

--- adminpanel/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con = mysqli_connect("localhost", "root", "", "sirohand");
-
-if (mysqli_connect_errno()) {
-    echo "Gagal Terkoneksi ke MySQL: " . mysqli_connect_error();
-    exit();
-}
-?>
