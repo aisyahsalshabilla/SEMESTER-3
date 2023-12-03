b10d114fced8e73ad76c1b8a2414c4a3d8bc7a9b
FitriAmaliaSaqila <mohonjangandihackyaa77@gmail.com>
2023-12-03 13:55:10 +0700

    logo

--- template/sidebar.php
+++ template/sidebar.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?= $main_url ?>dashboard.php" class="brand-link">
-      <img src="<?= $main_url ?>asset/image/logo.png" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="<?= $main_url ?>asset/image/emba.png" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">SIRohand</span>
     </a>
 
